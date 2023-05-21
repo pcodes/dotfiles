6b89e5e257d6c51224227f2aa94fd8014b74677e
Pat Cody <patcody028@gmail.com>
2023-05-21 13:13:54 -0700

    Minor LSP tinkering

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -26,33 +26,16 @@ require('mason-lspconfig').setup_handlers({
         require('lspconfig').bashls.setup({
             filetypes = { 'sh', 'zsh' }
         })
+    end,
+    ['yamlls'] = function()
+        require('lspconfig').yamlls.setup({
+            settings = {
+                yaml = {
+                    keyOrdering = false
+                }
+            }
+        })
     end
-    -- ["lua_ls"] = function()
-    --     require('lspconfig').lua_ls.setup({
-    --         settings = {
-    --             Lua = {
-    --                 runtime = {
-    --                     -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-    --                     version = 'LuaJIT',
-    --                 },
-    --                 diagnostics = {
-    --                     -- Get the language server to recognize the `vim` global
-    --                     globals = { 'vim' },
-    --                 },
-    --                 workspace = {
-    --                     -- Make the server aware of Neovim runtime files
-    --                     library = vim.api.nvim_get_runtime_file("", true),
-    --                     checkThirdParty = false
-    --                 },
-    --                 -- Do not send telemetry data containing a randomized but unique identifier
-    --                 telemetry = {
-    --                     enable = false,
-    --                 },
-    --             },
-    --             capabilities = capabilities
-    --         }
-    --     })
-    -- end
 })
 
 
