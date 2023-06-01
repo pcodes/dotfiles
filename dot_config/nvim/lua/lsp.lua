328f12ea6a1d127167c9721ce912d1661ec07a29
Pat Cody <patcody028@gmail.com>
2023-05-31 23:02:46 -0700

    Go back to using mason for all setup

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -4,6 +4,9 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities()
 M.capabilities = capabilities
 
+require("neodev").setup({
+})
+
 require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
@@ -16,28 +19,29 @@ require("mason-null-ls").setup({
     automatic_setup = true
 })
 
--- This sets up lua_ls and jsonls automatically
-require("neodev").setup({
-})
+require('mason-lspconfig').setup_handlers {
+    function(server_name)
+        require("lspconfig")[server_name].setup({
+            capabilities = capabilities
+        })
+    end,
+    ["bashls"] = function()
+        require("lspconfig").bashls.setup({
+            capabilities = capabilities,
+            filetypes = { 'sh', 'zsh' }
+        })
+    end,
+    ["yamlls"] = function()
+        require("lspconfig").yamlls.setup({
+            capabilities = capabilities,
+            settings = {
+                yaml = {
+                    keyOrdering = false
+                }
+            }
 
-local lspconfig = require('lspconfig')
-
-lspconfig.bashls.setup({
-    capabilities = capabilities,
-    filetypes = { 'sh', 'zsh' }
-})
-
-
-lspconfig.yamlls.setup({
-    capabilities = capabilities,
-    settings = {
-        yaml = {
-            keyOrdering = false
-        }
-    }
-
-})
-
-
+        })
+    end
+}
 
 return M
