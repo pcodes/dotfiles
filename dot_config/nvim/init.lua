986309e2a18e441b0a737fc3e8e0b47be0361765
Pat Cody <pat@patcody.io>
2023-06-14 18:20:53 -0700

    Misc fix to vim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -29,6 +29,8 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
     command = 'silent ! chezmoi apply --source-path %',
 })
 
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {pattern="*/waybar/config", command="set filetype=json"})
+
 -- Import all of the submodules
 require('settings')
 require("statusline")
