f7d83a858bff76377da66ecb439ba1dd1677e629
Pat Cody <pat@patcody.io>
2023-08-24 22:22:37 -0400

    Add config dir for nvim

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+local keymap = vim.keymap.set
+
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+local wk = require("which-key")
+wk.setup()
+
+local register_prefix = function(keybinding, name)
+    wk.register({
+        [keybinding] = { name = name }
+    })
+end
+
+-- ---Arrow key bindings---
+keymap("n", "<Up>", "<C-w><C-k>", { desc = 'Move cursor up window' })
+keymap("n", "<Down>", "<C-w><C-j>", { desc = 'Move cursor down window' })
+keymap("n", "<Left>", "<C-w><C-h>", { desc = 'Move cursor left window' })
+keymap("n", "<Right>", "<C-w><C-l>", { desc = 'Move cursor right window' })
+
