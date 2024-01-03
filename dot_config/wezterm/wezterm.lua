27c82b9b3ae84abc1c88de0595eddd83543582e9
Pat Cody <pat@patcody.io>
2024-01-02 19:19:21 -0500

    Add barebones wezterm setup

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("FiraCode Nerd Font")
+
+config.color_scheme = "Tokyo Night Moon"
+config.font_size = 18
+
+return config
